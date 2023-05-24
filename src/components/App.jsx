8f9b3e5f5f61c6794de52d4068f57288e3ec981f
Robi <robert.lazarov02@e-uvt.ro>
2023-05-24 21:38:00 +0300

    Changing dinamically notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import Heading from './Heading';
 import Footer from './Footer';
 import Note from './Note';
+import notes from "../notes.js"
+
+
 
 const App = () => {
     return ( 
         <div >
             <Heading />
-            <Note />
+                {notes.map((noteItem) => {
+                    return (<Note
+                        key = {noteItem.key}
+                        title = {noteItem.title}
+                        content = {noteItem.content}
+                    />
+                );
+})}
             <Footer />
         </div>
     );
